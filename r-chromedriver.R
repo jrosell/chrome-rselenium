51fcbb275b686368168f06277f9001d27e7fb37e
jordi <jroselln@gmail.com>
2022-08-25 15:12:38 +0200

    install = FALSE

--- r-chromedriver.R
+++ r-chromedriver.R
@@ -2,7 +2,7 @@ start_url <- "https://elnostreraco.com/blog/"
 url_path <- "https://elnostreraco.com/blog/"
 gh_required <- c("ropensci/binman", "ropensci/wdman", "ropensci/RSelenium")
 pacman::p_load_gh(gh_required, install = FALSE) # If it fails, install.packages("pacman") and set install = TRUE
-pacman::p_load(tidyverse)
+pacman::p_load(tidyverse, install = FALSE)
 # Adjust the proper chromedriver version according to your Google Chrome version (google-chrome --version)
 cDrv <- chrome(version = '104.0.5112.79')
 eCaps <- list(chromeOptions = list(args = c('--no-sandbox','--headless', '--disable-gpu', '--window-size=1280,800')))
